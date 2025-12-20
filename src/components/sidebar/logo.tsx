170d2c105af80a48cdba7b63b4c8531462eafb87
Desmond <desdhi24@gmail.com>
2025-12-20 10:35:05 +0000

    chore: worked on the frontend and file routing

--- src/components/sidebar/logo.tsx
+++ src/components/sidebar/logo.tsx
@@ -1,23 +1,30 @@
-import Image from 'next/image';
+// // import Image from 'next/image';
 
-import large from '@/../public/oryx-logo.png';
-import tiny from '@/../public/small-logo.png';
-import { useSidebar } from '@/components/ui';
+// import large from '@/../public/oryx-logo.png';
+// import tiny from '@/../public/small-logo.png';
+// import { useSidebar } from '@/components/ui';
 
-const SideLogo = () => {
-  const { state } = useSidebar();
+// const SideLogo = () => {
+//   const { state } = useSidebar();
 
-  return (
-    <div>
-      <Image
-        src={state === 'collapsed' ? tiny.src : large.src}
-        alt="logo"
-        width={100}
-        height={100}
-        className="invert"
-      />
-    </div>
-  );
+//   return (
+//     <div>
+//       <Image
+//         src={state === 'collapsed' ? tiny.src : large.src}
+//         alt="logo"
+//         width={100}
+//         height={100}
+//         className="invert"
+//       />
+//     </div>
+//   );
+// };
+
+// export default SideLogo;
+// import React from 'react'
+
+const logo = () => {
+  return <div>logo</div>;
 };
 
-export default SideLogo;
+export default logo;
