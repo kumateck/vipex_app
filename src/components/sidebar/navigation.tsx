bbdd162802791d2b2d7022e1ce9bbaf83c10c5bc
Banafo <surmjnr@gmail.com>
2026-03-18 12:17:26 +0000

    fix: added inventory to the the sidebar inventory/categories git rebase --continue

--- src/components/sidebar/navigation.tsx
+++ src/components/sidebar/navigation.tsx
@@ -285,6 +285,11 @@ export const ROUTES: Route[] = [
         title: 'Inventory Management',
         icon: 'Package2',
         items: [
+            {
+            title: 'Product Categories',
+            url: '/inventory/categories',
+            permissionKey: 'CanListProductCategories',
+          },
           {
             title: 'All Inventory',
             url: '/inventory',
