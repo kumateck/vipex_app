cd8fa99ff49e3c8339397ea8ace0b1844e39d900
Banafo <surmjnr@gmail.com>
2026-03-18 14:05:29 +0000

    fix: generated and edit and create import for location was touched, rerire later

--- src/pages/(private)/(configurations)/inventory/locations/edit/[id]/page.tsx
+++ src/pages/(private)/(configurations)/inventory/locations/edit/[id]/page.tsx
@@ -1,3 +1,4 @@
-import { InventoryLocationsEditPage } from '@/features/inventory';
+//revire this import to be more specific to inventory locations instead of categories
+import { InventoryCategoriesEditPage as InventoryLocationsEditPage } from '@/features/inventory/categories/pages/inventory-categories-edit-page';
 
 export default InventoryLocationsEditPage;
--- src/pages/(private)/(configurations)/inventory/locations/new/page.tsx
+++ src/pages/(private)/(configurations)/inventory/locations/new/page.tsx
@@ -1,3 +1,4 @@
-import { InventoryLocationsCreatePage } from '@/features/inventory';
+//revire this import to be more specific to inventory locations instead of categories
+import {InventoryCategoriesCreatePage as InventoryLocationsCreatePage } from '@/features/inventory';
 
 export default InventoryLocationsCreatePage;
